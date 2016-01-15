8b7c5bd4cd56793d20eb1c9c57e51187cabe992a
Alexey Eremin <a.eremin.msu@gmail.com>
2016-01-15 21:55:29 +0300

    Create OpenGL context. Fill background with some yellow color.

--- app/src/main/java/com/gmail/aereminmsu/zeptodemo/MainActivity.java
+++ app/src/main/java/com/gmail/aereminmsu/zeptodemo/MainActivity.java
@@ -1,13 +1,35 @@
 package com.gmail.aereminmsu.zeptodemo;
 
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private GLSurfaceView mGLSurfaceView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        mGLSurfaceView = new GLSurfaceView(this);
+        mGLSurfaceView.setEGLContextClientVersion(2);
+        mGLSurfaceView.setRenderer(new JavaRenderer());
+        setContentView(mGLSurfaceView);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        mGLSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        mGLSurfaceView.onPause();
     }
 }
+
